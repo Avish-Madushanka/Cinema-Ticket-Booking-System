770f15ce30e6173a592fe24248b1d8876deaa143
avish madushanka <avishmadushanka21@gmail.com>
2024-03-24 02:59:34 +0530

    create home page

---
+++ ticket-booking-system/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
