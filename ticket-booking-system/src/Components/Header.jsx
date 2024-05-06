6b22ef47d4d5b42609a287f4e474b0dd005a9067
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-06 20:46:01 +0530

    color

--- ticket-booking-system/src/Components/Header.jsx
+++ ticket-booking-system/src/Components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
             <a href="/About" class="block py-2 px-3 text-black-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Us</a>
           </li>
           <li>
-            <a href="/Contact" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
+            <a href="/Contact" class="block py-2 px-3 text-black-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
           </li>
          
            
