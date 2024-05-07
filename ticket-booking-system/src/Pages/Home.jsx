560095f37f7b12d88bbd664f75593dc15627e087
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-07 20:35:21 +0530

    hero sub text

--- ticket-booking-system/src/Pages/Home.jsx
+++ ticket-booking-system/src/Pages/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
                 </svg>
             </a>
             <a href="#" class="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-70">
-                Learn more
+                Contact Us
             </a>  
         </div>
     </div>
