e05cedc1a32f11520753ac2c2fe240e71c142269
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-07 20:23:15 +0530

    hero sub tex

--- ticket-booking-system/src/Pages/Home.jsx
+++ ticket-booking-system/src/Pages/Home.jsx
@@ -44,10 +44,10 @@ const Home = () => {
 </section>
 
 
-<section class="bg-white dark:bg-gray-900 mt-16">
+<section class="bg-white dark:bg-gray-900 mt-8">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Your Best Cinematic Partner!</h1>
-        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Cinema hall bookings are now convienient, fast and at your finger tips!</p>
+        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Cinema hall bookings are now convienient, fast and at your finger tips! Book your fav movie now with no hassles!</p>
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Get started
