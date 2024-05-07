9992343c8c2867441c3ec2190c0bc9cae80b8ba6
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-07 20:17:18 +0530

    changed hero title
    Co-authored-by: Avish-Madushanka <avishmadushanka21@gmail.com>

--- ticket-booking-system/src/Pages/Home.jsx
+++ ticket-booking-system/src/Pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
 
 <section class="bg-white dark:bg-gray-900 mt-16">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Your Best Cinematic Partner!</h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
